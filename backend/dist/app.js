22e4422fba3d0c43cff4f1f91775510f0513c36c
NASEELA K T <naslalellu@gmail.com>
2024-05-04 01:29:19 +0530

    package main changes

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -15,6 +15,7 @@ const otpExpirationMiddleware_1 = require("./middlewares/otpExpirationMiddleware
 const cookieParser = require("cookie-parser");
 const messageRoutes_1 = __importDefault(require("./routes/messageRoutes"));
 const conversationRoutes_1 = __importDefault(require("./routes/conversationRoutes"));
+const path_1 = __importDefault(require("path"));
 const socket_1 = __importDefault(require("./socket"));
 const http_1 = require("http");
 const app = (0, express_1.default)();
@@ -22,9 +23,11 @@ dotenv_1.default.config();
 (0, connectToMongoDB_1.connectDB)();
 const server = (0, http_1.createServer)(app);
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:5000"],
+    origin: ["http://localhost:5000", "https://eventcastle.online"],
     credentials: true
 }));
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
+app.use(express_1.default.static(path_1.default.join(__dirname, '../../frontend/dist')));
 const sessionMiddleware = (0, express_session_1.default)({
     secret: 'cfcyygyv',
     saveUninitialized: true,
@@ -47,7 +50,15 @@ app.use('/api/user', userRoutes_1.default);
 app.use('/api/vendor', vendorRoutes_1.default);
 app.use('/api/messages', messageRoutes_1.default);
 app.use('/api/conversation', conversationRoutes_1.default);
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ error: 'Unauthorized' });
+    }
+});
 (0, socket_1.default)(server);
+app.get('*', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '../../frontend/dist/index.html'));
+});
 const PORT = process.env.PORT;
 server.listen(PORT, () => {
     console.log(`Server running on ${PORT}...`);
